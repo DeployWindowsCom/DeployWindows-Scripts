8edc2486f81ce965c3938d3cfccccacec70fa315
Mattias Fors <11193779+DeployWindowsCom@users.noreply.github.com>
2018-12-10 23:57:21 +0100

    donotshowfeedbacknotifications added

--- Intune/Windows/TelemetryCustomization.ps1
+++ Intune/Windows/TelemetryCustomization.ps1
@@ -5,7 +5,7 @@
 #
 #Version
 # 1.0  First release
-# 1.1 Updated for 64-bit system
+# 1.2  Added DoNotShowFeedbackNotification
 #
 ##############################
 
@@ -22,21 +22,17 @@ $Is64OS = $false; if (($env:PROCESSOR_ARCHITEW6432 -like "AMD64") -or ($env:PROC
 
 if (($Is64OS) -and (-not $Is64Bit)) {
     # Running AMD64 but no AMD64 Process, Restart script
-    Write-Host "Running AMD64 OS and x86 environment, restart script"
     $Invocation = $PSCommandPath
     if ($Invocation -eq $null) { return }
     $SysNativePath = $PSHOME.ToLower().Replace("syswow64", "sysnative")
-    $Ret = Start-Process "$SysNativePath\powershell.exe" -ArgumentList "-ex ByPass -file `"$Invocation`" " -WindowStyle normal -PassThru -Wait
+    $Ret = Start-Process "$SysNativePath\powershell.exe" -ArgumentList "-ex ByPass -file `"$Invocation`" " -WindowStyle Hidden -PassThru -Wait -RedirectStandardOutput $(Out-Host)
     return $Ret.ExitCode;
 } elseif ((-not $Is64OS) -and (-not $Is64Bit)) {
     #Running x86 and no AMD64 Process, Do not bother restarting
-    Write-Host "Running x86 OS and x86 environment, continue"
 }
 #endregion
 
 #region Your content goes here
-
-Write-Host "64-Bit Environment: $($Is64Bit) on 64-Bit Windows: $($Is64OS)"
 
 $TelemetryReg = "HKLM:\SOFTWARE\Policies\Microsoft\Windows\DataCollection"
 
@@ -44,10 +40,8 @@ if((Test-Path -Path $TelemetryReg) -eq $false) {
     New-Item -Path $TelemetryReg -ItemType Key
 }
 
-
 New-ItemProperty -Path $TelemetryReg -Name AllowDeviceNameInTelemetry -PropertyType DWord -Value 1 -Force -ErrorAction Continue
 New-ItemProperty -Path $TelemetryReg -Name DisableTelemetryOptInChangeNotification -PropertyType DWord -Value 1 -Force -ErrorAction Continue
-
-Exit 0
+New-ItemProperty -Path $TelemetryReg -Name DoNotShowFeedbackNotifications -PropertyType DWord -Value 1 -Force -ErrorAction Continue
 
 #endregion
