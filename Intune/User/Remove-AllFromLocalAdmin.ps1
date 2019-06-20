7bf877124d309032299a29b209fd43c89206091b
Mattias Fors <11193779+DeployWindowsCom@users.noreply.github.com>
2019-06-21 00:09:20 +0200

    remove users

---
+++ Intune/User/Remove-AllFromLocalAdmin.ps1
@@ -0,0 +1,33 @@
+﻿$excludeUsers = @( 
+        "Administratör", 
+        "username1")
+
+# Translate the S-1-5-32-544 (.\Administrators) SID to a group name, the name varies depending on the language version of Windows.
+$sid = 'S-1-5-32-544'
+$objSID = New-Object System.Security.Principal.SecurityIdentifier($sid)
+$localadminsgroup = (( $objSID.Translate([System.Security.Principal.NTAccount]) ).Value).Split("\")[1]
+
+$group = [ADSI]("WinNT://$($env:COMPUTERNAME)/$($localadminsgroup),group")
+foreach ($user in $group.Members() )
+{
+    $adsPath = $user.GetType().InvokeMember('Adspath', 'GetProperty', $null, $user, $null)
+    $username = $adsPath.Split('/', [System.StringSplitOptions]::RemoveEmptyEntries)[-1]
+    $domain = $adsPath.Split('/', [System.StringSplitOptions]::RemoveEmptyEntries)[-2]
+
+    if ($excludeUsers -contains $username)
+    {
+        Write-Host "Do not remove $($username)"
+    }
+    elseif ($domain -ne "WinNT:")
+    {
+        Write-Host "Remove the user $($username)"
+        try
+        {
+            $group.Remove("WinNT://$($env:COMPUTERNAME)/$($domain)/$($username)")
+        }
+        catch
+        {
+            Write-Host "Trying to remove user $($username) $($_.Exception.Message)" -BackgroundColor Red
+        }
+    }
+}
---
+++ Intune/User/Remove-InteractiveFromAdmin.ps1
@@ -0,0 +1,21 @@
+﻿# Translate the S-1-5-32-544 (.\Administrators) SID to a group name, the name varies depending on the language version of Windows.
+$sid2 = 'S-1-5-32-544'
+$objSID2 = New-Object System.Security.Principal.SecurityIdentifier($sid2)
+$localadminsgroup = (( $objSID2.Translate([System.Security.Principal.NTAccount]) ).Value).Split("\")[1]
+
+# Translate the S-1-5-4 (NT AUTHORITY\Interactive) SID to an account name, the name varies depending on the language version of Windows.
+$sid1 = 'S-1-5-4'
+$auth = New-Object System.Security.Principal.SecurityIdentifier($sid1) 
+$interactive = $auth.Translate([System.Security.Principal.NTAccount])
+
+
+# Add the security principal name to the local administrators group. (used old style of adding group members due to compatibility reasons)
+
+try {
+    Write-Host "Adding security principal: $interactive to the $localadminsgroup group..."
+
+    net localgroup $localadminsgroup $interactive /delete
+}
+Catch {
+    write-host $_.Exception.Message
+}
