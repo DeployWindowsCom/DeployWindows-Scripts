83e38c9a8700634480be082dfa1af17df0dd03c1
Mattias Fors <11193779+DeployWindowsCom@users.noreply.github.com>
2021-10-05 21:14:13 +0200

    update printer win32

--- Intune/Apps/Printer/detect.ps1
+++ Intune/Apps/Printer/detect.ps1
@@ -4,18 +4,18 @@ $Printer = "\\server.domain.com\printer01"
 
 # Checking print driver
 if (@(Get-PrinterDriver -Name $PrintDriverName).Count -le 0) { 
-	Write-Host "No print driver installed"
+	Write-Output "No print driver installed"
 	Exit 1
 } else {
-	Write-Host "Print driver installed"
+	Write-Output "Print driver installed"
 }
 
 # Adding printer
 if (@(Get-Printer -Name $printer).Count -le 0) { 
-	Write-Host "No printer installed"
+	Write-Output "No printer installed"
 	Exit 2
 } else {
-	Write-Host "Printer installed"
+	Write-Output "Printer installed"
 }
 
 Exit 0
